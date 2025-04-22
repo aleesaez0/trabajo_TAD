1a3dacb394ac030881d96109810eb5998986c035
joselulg03 <103925590+joselulg03@users.noreply.github.com>
2025-04-22 19:38:59 +0200

    Update catalogo.blade.php
    Closes #7

--- resources/views/productos/catalogo.blade.php
+++ resources/views/productos/catalogo.blade.php
@@ -8,17 +8,28 @@
                 <div class="col-md-4">
                     <div class="card h-100 shadow-sm">
                         @if ($producto->imagen)
-                            <img src="{{ $producto->imagen }}" class="card-img-top" alt="{{ $producto->nombre }}">
+                            <div class="img-wrapper">
+                                <img src="{{ $producto->imagen }}" class="card-img-top" alt="{{ $producto->nombre }}">
+                            </div>
                         @endif
-                        <div class="card-body">
+                        <div class="card-body d-flex flex-column">
                             <h5 class="card-title">{{ $producto->nombre }}</h5>
-                            <p class="card-text">{{ $producto->descripcion }}</p>
-                            <p class="fw-bold">{{ $producto->precio }} €</p>
+
+                            <p class="card-text descripcion">
+                                {{ Str::limit($producto->descripcion, 100, '...') }}
+                                @if(strlen($producto->descripcion) > 100)
+                                    <span class="ver-mas"
+                                        onclick="this.previousSibling.textContent = '{{ addslashes($producto->descripcion) }}'; this.style.display='none'">Ver
+                                        más</span>
+                                @endif
+                            </p>
+
+                            <p class="fw-bold mt-auto">{{ $producto->precio }} €</p>
 
                             <form method="POST" action="{{ route('carro.agregar', $producto) }}">
                                 @csrf
                                 <input type="hidden" name="cantidad" value="1">
-                                <button class="btn btn-success w-100">Añadir al carro</button>
+                                <button class="btn btn-success w-100 mt-2">Añadir al carro</button>
                             </form>
                         </div>
                     </div>
