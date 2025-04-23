8b8c83b1946adaf0e7b04ae590a2f959b3d1d86c
joselulg03 <103925590+joselulg03@users.noreply.github.com>
2025-04-23 19:10:34 +0200

    Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,24 +10,9 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Crear usuario administrador
-        $adminUser = User::create([
-            'name' => 'Admin Demo',
-            'email' => 'admin@admin.com',
-            'password' => Hash::make('admin123'),
+        $this->call([
+            ProductoSeeder::class,
+            UsuarioClienteAdminSeeder::class,
         ]);
-
-        // Crear usuario cliente
-        $clienteUser = User::create([
-            'name' => 'Cliente Demo',
-            'email' => 'cliente@cliente.com',
-            'password' => Hash::make('cliente123'),
-        ]);
-
-        // Llamar a los seeders y pasar user_id
-        $this->callWith(AdministradorSeeder::class, ['user_id' => $adminUser->id]);
-        $this->callWith(ClienteSeeder::class, ['user_id' => $clienteUser->id]);
-
-        $this->call(ProductoSeeder::class);
     }
 }
