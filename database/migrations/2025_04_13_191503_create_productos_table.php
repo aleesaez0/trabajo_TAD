c440a96f6816c90f96212a9b8e4b112997b60584
joselulg03 <103925590+joselulg03@users.noreply.github.com>
2025-05-05 17:30:47 +0200

    Update 2025_04_13_191503_create_productos_table.php

--- database/migrations/2025_04_13_191503_create_productos_table.php
+++ database/migrations/2025_04_13_191503_create_productos_table.php
@@ -17,7 +17,7 @@ return new class extends Migration {
             $table->decimal('precio', 10, 2);
             $table->integer('stock')->default(0);
             $table->string('imagen')->nullable();
-            $table->foreignId('categoria_id')->constrained('categorias')->onDelete('cascade');
+            $table->foreignId('categoria_id')->constrained('categorias')->onDelete('cascade')->default(null);
             $table->timestamps();
         });
     }
