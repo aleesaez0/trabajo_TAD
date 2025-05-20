df9a2422b9fc0f88969e92f7e3641c38adae5835
José Luis Leal García <joseluislealgarcia03@gmail.com>
2025-05-20 18:55:58 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
+#!/usr/bin/env sh
 set -e
 
 echo "Instalando dependencias PHP (Composer)…"
 composer install --prefer-dist --no-interaction --optimize-autoloader
 
 echo "Esperando a que la base de datos esté lista en ${DB_HOST}:${DB_PORT:-3306}…"
+
 until nc -z "${DB_HOST}" "${DB_PORT:-3306}"; do
   echo "Esperando DB…"
   sleep 1
